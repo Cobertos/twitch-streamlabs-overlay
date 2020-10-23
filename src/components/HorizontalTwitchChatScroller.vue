78c93906f41a50c8b857379c0e4e8b6232cb376c
cobertos <me+git@cobertos.com>
2020-10-23 06:57:37 -0400

    Got twitch chat working (though I removed the token from commit)

---
+++ src/components/HorizontalTwitchChatScroller.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    
+  </div>
+</template>
+
+<script>
+import { ChatClient } from 'twitch-chat-client';
+//import { ApiClient } from 'twitch';
+import { StaticAuthProvider } from 'twitch-auth';
+
+const clientId = 'e7g44jusrmcfqyl59kzxe2j4c7ud9g';
+const accessToken = '';
+const authProvider = new StaticAuthProvider(clientId, accessToken);
+//const apiClient = new ApiClient({ authProvider });
+const chatClient = new ChatClient(authProvider, { channels: ['robotfrogs3'] });
+
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String
+  },
+  async mounted() {
+    await chatClient.connect();
+    console.log("Connected");
+    chatClient.onMessage(console.log);
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
