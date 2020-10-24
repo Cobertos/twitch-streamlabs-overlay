e14e254b9d06798a47e85c50654b6c0e52252e96
cobertos <me+git@cobertos.com>
2020-10-24 01:46:13 -0400

    Use correct data passing for POST

--- api/codeToToken.js
+++ api/codeToToken.js
@@ -7,11 +7,18 @@ module.exports = async (req, res) => {
   const code = req.query.code;
 
   // Take code from request
-  const resp = await fetch(`https://streamlabs.com/api/v1.0/token?grant_type=authorization_code&client_id=${clientID}&client_secret=${clientSecret}&redirect_url=${redirectURI}&code=${code}`, {
-    method: 'POST'
+  const resp = await fetch(`https://streamlabs.com/api/v1.0/token`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: `grant_type=authorization_code&client_id=${clientID}&client_secret=${clientSecret}&redirect_url=${redirectURI}&code=${code}`
   });
   const json = await resp.json();
-  console.log(json);
+  if(!json.access_code) {
+    console.error(json);
+    throw new Error('No access_code was obtained');
+  }
 
   res.json({
     access_code: json.access_code,
